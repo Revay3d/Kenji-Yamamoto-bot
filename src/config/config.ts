805da66573dcf8c75dc97c6671d2f927e24c9033
Yasu Yumiko <86036041+Yumiko0828@users.noreply.github.com>
2022-07-13 19:47:28 +0000

    update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,8 @@ config();
 
 export default {
   token: process.env.TOKEN, // Your token here
-  clientId: "", // Your Client Id here
-  guildId: "", // Your Guild Id here
-  prefix: "$", // Your prefix here
+  clientId: "977232891119558697", // Your Client Id here
+  guildId: "975079248840769586", // Your Guild Id here
+  prefix: "+", // Your prefix here
   version, // The version of the bot here
 };
